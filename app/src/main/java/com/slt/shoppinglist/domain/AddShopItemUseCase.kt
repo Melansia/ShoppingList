550abe9560be2e1c8f2e850ef36cc3f6009fcc9d
Grigori L <lupasco.grigori@gmail.com>
2022-07-13 17:12:18 +0300

    setting up domain layer

---
+++ app/src/main/java/com/slt/shoppinglist/domain/AddShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.slt.shoppinglist.domain
+
+class AddShopItemUseCase {
+
+    fun addShopItem(shopItem: ShopItem) {
+
+    }
+}
---
+++ app/src/main/java/com/slt/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package com.slt.shoppinglist.domain
+
+class GetShopListUseCase {
+
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+}
