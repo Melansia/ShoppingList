093a1fc4a4dcd9665f60d66bb88b15692b777412
Grigori L <lupasco.grigori@gmail.com>
2022-07-17 20:30:43 +0300

    create ShopListAdapter

--- app/src/main/java/com/slt/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/slt/shoppinglist/presentation/MainViewModel.kt
@@ -24,7 +24,7 @@ class MainViewModel: ViewModel() {
         deleteShopItemUseCase.deleteShopItem(shopItem)
     }
 
-    fun editShopItem(shopItem: ShopItem) {
+    fun changeEnableState(shopItem: ShopItem) {
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
         editShopItemUseCase.editShopItem(newItem)
     }
