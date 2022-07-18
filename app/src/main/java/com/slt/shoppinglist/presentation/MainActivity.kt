957c9a3eb375f07d1a7e9fd3dbf16e147efb9748
Grigori L <lupasco.grigori@gmail.com>
2022-07-18 21:34:45 +0300

    recyclerView test and bugfix

--- app/src/main/java/com/slt/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/slt/shoppinglist/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.slt.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
@@ -14,16 +15,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var viewModel: MainViewModel
-
-    private var count = 0
+    private lateinit var adapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
 //            Log.d(TAG, it.toString())
+            adapter.shopList = it
         }
+    }
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
     }
 }
