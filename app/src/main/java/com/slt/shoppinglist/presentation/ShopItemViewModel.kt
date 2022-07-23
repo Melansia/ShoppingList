3597247f406c7064455891d0e2002e984e8441e3
Grigori L <lupasco.grigori@gmail.com>
2022-07-23 13:48:02 +0300

    prepare layout ShopItemActivity

--- app/src/main/java/com/slt/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/slt/shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.slt.shoppinglist.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.slt.shoppinglist.data.ShopListRepositoryImpl
 import com.slt.shoppinglist.domain.AddShopItemUseCase
@@ -15,8 +17,25 @@ class ShopItemViewModel : ViewModel() {
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
     fun getShopItem(shopItemId: Int) {
         val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
     }
 
     fun addShopItem(inputName: String?, inputCount: String?) {
@@ -26,6 +45,7 @@ class ShopItemViewModel : ViewModel() {
         if (fieldsValid) {
             val shopItem = ShopItem(name, count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            workDoneCloseScreen()
         }
     }
 
@@ -36,13 +56,20 @@ class ShopItemViewModel : ViewModel() {
         if (fieldValid) {
             val shopItem = ShopItem(name, count, true)
             editShopItemUseCase.editShopItem(shopItem)
+            workDoneCloseScreen()
         }
     }
 
     private fun validateInput(name: String, count: Int): Boolean {
         var result = true
-        if (name.isBlank()) result = false
-        if (count <= 0) result = false
+        if (name.isBlank()) {
+            _errorInputName.value = true
+            result = false
+        }
+        if (count <= 0) {
+            _errorInputCount.value = true
+            result = false
+        }
 
         return result
     }
@@ -57,5 +84,17 @@ class ShopItemViewModel : ViewModel() {
         } catch (e: Exception) {
             0
         }
+    }
+
+    public fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    public fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun workDoneCloseScreen() {
+        _shouldCloseScreen.value = Unit
     }
 }
